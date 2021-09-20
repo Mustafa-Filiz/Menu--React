c76a50223229e7945e9635ba64f4b7bbe0355b8a
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-09-20 18:26:23 +0300

    completed template

--- src/Review.js
+++ src/Review.js
@@ -1,3 +1,5 @@
+import { ArrowBackIosRounded, ArrowForwardIosRounded, FormatQuoteRounded } from '@mui/icons-material';
+import { Paper } from '@mui/material';
 import React, { useState } from 'react';
 import people from './data';
 // import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
@@ -6,7 +8,20 @@ const Review = () => {
   const [index, setIndex] = useState(0)
   const {name, job, image, text} = people[index]
   return (
-    <h2>review component</h2>
+      <div className="review">
+        <div className="img-container">
+          <img src={image} alt="pp" className="person-img" />
+          <FormatQuoteRounded fontSize="large" className="quote-icon"/>
+        </div>
+        <h3 className="author">{name}</h3>
+        <h4 className="job">{job}</h4>
+        <p className="info">{text}</p>
+        <div>
+          <button className="prev-btn" ><ArrowBackIosRounded /></button>
+          <button className="next-btn" ><ArrowForwardIosRounded /></button>
+        </div>
+        <button className="random-btn">Surprise Me</button>
+      </div>
   );
 };
 
