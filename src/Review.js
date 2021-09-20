46fd67926d83d3514a28a39c80029eb58d693cdb
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-09-20 17:28:15 +0300

    first commit

---
+++ src/Review.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import people from './data';
+// import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+const Review = () => {
+  const [index, setIndex] = useState(0)
+  const {name, job, image, text} = people[index]
+  return (
+    <h2>review component</h2>
+  );
+};
+
+export default Review;
